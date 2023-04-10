74eb6c6a504155e46cf1e5b7074c4fe4d5ab9130
Kanishka Ghosh <91798916+KanishkaGhosh21@users.noreply.github.com>
2023-04-10 21:04:16 +0530

    testing bg

--- app/main.py
+++ app/main.py
@@ -167,6 +167,16 @@ def load_image():
 
 
 def main():
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: cover;
+    }
+    </style>
+    '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    
     st.header('TARP Final Review')
     st.title('MRI Brain Tumor Detection')
     st.subheader('Presented by')
